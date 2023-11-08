f0046149daa8085b3b356a348dd5212a1873a087
Mamn847 <133536689+Mamn847@users.noreply.github.com>
2023-11-08 10:51:09 +0200

    test

--- ALTC-Site/Models/ErrorViewModel.cs
+++ ALTC-Site/Models/ErrorViewModel.cs
@@ -3,7 +3,9 @@ namespace ALTC_Site.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+/// <summary>
+/// ////////////
+/// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
