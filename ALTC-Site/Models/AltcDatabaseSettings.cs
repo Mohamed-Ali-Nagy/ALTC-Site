e9122f83de37fd33200e41a553f143d1649e5cd0
mohamed <mohamedhakk67@gmail.com>
2023-11-08 13:27:36 +0200

    Add Admin Dashboard

---
+++ ALTC-Site/Models/AltcDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace ALTC_Website.Models
+{
+    public class AltcDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string TechnicalCollection { get; set; } = null!;
+        public string JobCandidateCollection { get; set; } = null!;
+        public string StaticCollection { get; set; } = null!;
+        public string RequestCollection { get; set; }
+        public string ComplainCollection { get; set; }
+
+    }
+}
